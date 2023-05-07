f05a0731d7da97f9b27741a26b7e635d4cb7982e
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-07 16:40:09 +0200

    creating repo FilRougeFo

---
+++ src/main/java/com/example/filrougefo/FilRougeFoApplication.java
@@ -0,0 +1,13 @@
+package com.example.filrougefo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeFoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilRougeFoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/filrougefo/FilRougeFoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.filrougefo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRougeFoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
