ba8a4c663822fd23139cb86f372685516c47785b
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-08 18:06:55 +0200

    added : findaddressbyclientid method

--- src/main/java/com/example/filrougefo/repository/AddressRepository.java
+++ src/main/java/com/example/filrougefo/repository/AddressRepository.java
@@ -4,6 +4,9 @@ import com.example.filrougefo.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
+    List<Address> findAddressByClient_Id(Long id);
 }
