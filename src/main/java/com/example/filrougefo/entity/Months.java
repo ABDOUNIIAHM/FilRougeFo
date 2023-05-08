3ae710fbb935ca05c7af8f69875b88b5d00dfa11
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-08 20:33:06 +0200

    fix: changing name of class Month ->Months because of confusion with Month java class

--- src/main/java/com/example/filrougefo/entity/Months.java
+++ src/main/java/com/example/filrougefo/entity/Months.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Immutable
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
-public class Month {
+public class Months {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
