9c409b1e7fa1f5a7a69f14c2ab604d409c60004d
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-14 13:26:06 +0200

    added findAllCategoriesExceptProductCategory service

--- src/main/java/com/example/filrougefo/service/category/CategoryService.java
+++ src/main/java/com/example/filrougefo/service/category/CategoryService.java
@@ -1,11 +1,14 @@
 package com.example.filrougefo.service.category;
 import com.example.filrougefo.entity.Category;
+import com.example.filrougefo.entity.Product;
 import com.example.filrougefo.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class CategoryService implements IntCategoryService{
@@ -32,5 +35,19 @@ public class CategoryService implements IntCategoryService{
                 .orElseThrow(()->new RuntimeException("No category found for: "+name));
 
         return searchedCategories;
+    }
+
+    @Override
+    public List<Category> findAllCategoriesExceptProductCategory(Product product) {
+
+        List<Category> categories =
+
+                categoryRepository
+                .findAll()
+                .stream()
+                .filter(category -> category.getId() != product.getCategory().getId())
+                .collect(Collectors.toList());
+
+        return categories;
     }
 }
--- src/main/java/com/example/filrougefo/service/category/IntCategoryService.java
+++ src/main/java/com/example/filrougefo/service/category/IntCategoryService.java
@@ -1,5 +1,6 @@
 package com.example.filrougefo.service.category;
 import com.example.filrougefo.entity.Category;
+import com.example.filrougefo.entity.Product;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -9,5 +10,6 @@ public interface IntCategoryService {
     List<Category> findAll();
     Category findById(int id);
     List<Category> findBySearchedName(String name);
+    List<Category> findAllCategoriesExceptProductCategory(Product product);
 
 }
