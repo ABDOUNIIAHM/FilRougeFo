d21d57aead7add03d473acd8aaceaf5143d78403
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-14 17:17:35 +0200

    added searchByName service in productService

--- src/main/java/com/example/filrougefo/repository/ProductRepository.java
+++ src/main/java/com/example/filrougefo/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.example.filrougefo.repository;
 
+import com.example.filrougefo.entity.Category;
 import com.example.filrougefo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Optional<List<Product>> findAllByNameContainingIgnoreCase(String name);
+
 }
--- src/main/java/com/example/filrougefo/service/product/ProductService.java
+++ src/main/java/com/example/filrougefo/service/product/ProductService.java
@@ -1,7 +1,32 @@
 package com.example.filrougefo.service.product;
-
+import com.example.filrougefo.entity.Product;
+import com.example.filrougefo.repository.ProductRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@AllArgsConstructor
 public class ProductService implements IntProductService{
+    private final ProductRepository productRepository;
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+    @Override
+    public Product findById(int id) {
+
+        return productRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("No product with id="+id+" was found !"));
+    }
+    @Override
+    public List<Product> searchProductByNamePattern(String name) {
+
+        return productRepository
+                .findAllByNameContainingIgnoreCase(name)
+                .orElseThrow(()-> new RuntimeException("No product with name="+name+" was found !"));
+    }
 }
