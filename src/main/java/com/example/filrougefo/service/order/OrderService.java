52d47c39b3bf483e0981abad0e42244244c5127d
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-15 21:17:33 +0200

    added customized exception for Order + updated methods repo and service

--- src/main/java/com/example/filrougefo/service/order/OrderService.java
+++ src/main/java/com/example/filrougefo/service/order/OrderService.java
@@ -1,13 +1,44 @@
 package com.example.filrougefo.service.order;
 
+import com.example.filrougefo.entity.Order;
+import com.example.filrougefo.exception.OrderNotFoundException;
 import com.example.filrougefo.repository.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class OrderService implements IntOrderService{
     private final OrderRepository orderRepository;
 
+    @Override
+    public List<Order> findAllOrdersByClientId(long id) {
+        return orderRepository.findAllByClientId(id);
+    }
 
+    @Override
+    public List<Order> findAllOrdersByStatus_Id(long id) {
+        return orderRepository.findAllByStatus_Id(id);
+    }
+
+    @Override
+    public List<Order> findAllOrdersByStatus_Name(String name) {
+        return orderRepository.findAllByStatus_Name(name);
+    }
+
+    @Override
+    public List<Order> findAllByOrdersBeforeChosenDate(Date date) {
+        return orderRepository.findAllByDateIsLessThanEqual(date);
+    }
+
+    @Override
+    public Order findOrderById(long id) throws OrderNotFoundException {
+        return orderRepository
+                .findById(id)
+                .orElseThrow(()-> new OrderNotFoundException("No Order found for id:"+id));
+    }
 }
