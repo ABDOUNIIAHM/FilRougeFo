ee7d44ddc2eb081a915a9780fb302643f87a7ebe
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-15 17:34:49 +0200

    added methods to Order repo service

--- src/main/java/com/example/filrougefo/repository/OrderRepository.java
+++ src/main/java/com/example/filrougefo/repository/OrderRepository.java
@@ -4,6 +4,11 @@ import com.example.filrougefo.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
+    List<Order> findAllByClientId(long id);
+    List<Order> findAllByStatus_Id(long id);
+    List<Order> findAllByStatus_Name(String name);
 }
--- src/main/java/com/example/filrougefo/service/order/OrderService.java
+++ src/main/java/com/example/filrougefo/service/order/OrderService.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class OrderService implements IntOrderService{
     private final OrderRepository orderRepository;
+
+
 }
