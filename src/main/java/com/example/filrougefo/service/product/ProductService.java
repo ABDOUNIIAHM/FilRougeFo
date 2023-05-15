7b592c15fdf950e5348097662a88eef44150d269
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-15 20:43:46 +0200

    added new custumised exceptions for Product

--- src/main/java/com/example/filrougefo/service/product/ProductService.java
+++ src/main/java/com/example/filrougefo/service/product/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.filrougefo.service.product;
 import com.example.filrougefo.entity.Product;
+import com.example.filrougefo.exception.ProductNotFoundException;
 import com.example.filrougefo.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,17 +17,17 @@ public class ProductService implements IntProductService{
         return productRepository.findAll();
     }
     @Override
-    public Product findById(int id) {
+    public Product findById(int id) throws ProductNotFoundException {
 
         return productRepository
                 .findById(id)
-                .orElseThrow(() -> new RuntimeException("No product with id="+id+" was found !"));
+                .orElseThrow(() -> new ProductNotFoundException("No product with id="+id+" was found !"));
     }
     @Override
-    public List<Product> searchProductByNamePattern(String name) {
+    public List<Product> searchProductByNamePattern(String name) throws ProductNotFoundException {
 
         return productRepository
                 .findAllByNameContainingIgnoreCase(name)
-                .orElseThrow(()-> new RuntimeException("No product with name="+name+" was found !"));
+                .orElseThrow(()-> new ProductNotFoundException("No product with name="+name+" was found !"));
     }
 }
