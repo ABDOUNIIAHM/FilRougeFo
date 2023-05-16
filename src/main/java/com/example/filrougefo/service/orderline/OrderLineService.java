fce948e1e12f84996104d475100a987d0585a8cf
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-16 12:36:56 +0200

    added methods to OrderRepository and service interface

--- src/main/java/com/example/filrougefo/service/orderline/OrderLineService.java
+++ src/main/java/com/example/filrougefo/service/orderline/OrderLineService.java
@@ -1,12 +1,30 @@
 package com.example.filrougefo.service.orderline;
 
+import com.example.filrougefo.entity.OrderLine;
 import com.example.filrougefo.repository.OrderLineRepository;
 import com.example.filrougefo.service.order.IntOrderService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class OrderLineService implements IntOrderLineService {
     private final OrderLineRepository orderLineRepository;
+
+    @Override
+    public OrderLine findById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<OrderLine> findAllOrderLinesByOrderId(long idOrder) {
+        return null;
+    }
+
+    @Override
+    public OrderLine findOrderLineByOrderIdAndProductId(long idOrder, long idProduct) {
+        return null;
+    }
 }
