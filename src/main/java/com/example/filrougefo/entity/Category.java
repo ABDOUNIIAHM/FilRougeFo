97855c41614fbc33b5524e31ae49310cd8b940f5
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-16 20:13:28 +0200

    added category controller

--- src/main/java/com/example/filrougefo/entity/Category.java
+++ src/main/java/com/example/filrougefo/entity/Category.java
@@ -16,7 +16,7 @@ public class Category {
     private int id;
     @Column(unique = true)
     private String name;
-    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private List<Product> products = new ArrayList<>();
 
---
+++ src/main/java/com/example/filrougefo/web/category/CategoryController.java
@@ -0,0 +1,4 @@
+package com.example.filrougefo.web.category;
+
+public class CategoryController {
+}
