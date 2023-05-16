0c93abb9a9059a2fd7585c39825f2816e3b0b231
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-16 08:50:37 +0200

    added exception handler methods for product and order

--- src/main/java/com/example/filrougefo/exception/handler/CategoryExceptionHandler.java
+++ src/main/java/com/example/filrougefo/exception/handler/CategoryExceptionHandler.java
@@ -2,6 +2,8 @@ package com.example.filrougefo.exception.handler;
 
 import com.example.filrougefo.RestController.CategoryController;
 import com.example.filrougefo.exception.CategoryControllerException;
+import com.example.filrougefo.exception.OrderControllerException;
+import com.example.filrougefo.exception.ProductControllerException;
 import com.example.filrougefo.exception.dto.DtoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,32 @@ import java.time.ZonedDateTime;
 public class CategoryExceptionHandler {
     @ExceptionHandler(value = {CategoryControllerException.class})
     public ResponseEntity<Object> handleCategoryRequestException(CategoryControllerException e){
+
+        HttpStatus badRequest = HttpStatus.NOT_FOUND;
+        DtoException dtoException = new DtoException(
+                e.getMessage(),
+                //e,
+                HttpStatus.NOT_FOUND,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+        return new ResponseEntity<>(dtoException, badRequest);
+
+    }
+    @ExceptionHandler(value = {ProductControllerException.class})
+    public ResponseEntity<Object> handleProductRequestException(ProductControllerException e){
+
+        HttpStatus badRequest = HttpStatus.NOT_FOUND;
+        DtoException dtoException = new DtoException(
+                e.getMessage(),
+                //e,
+                HttpStatus.NOT_FOUND,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+        return new ResponseEntity<>(dtoException, badRequest);
+
+    }
+    @ExceptionHandler(value = {OrderControllerException.class})
+    public ResponseEntity<Object> handleOrderRequestException(OrderControllerException e){
 
         HttpStatus badRequest = HttpStatus.NOT_FOUND;
         DtoException dtoException = new DtoException(
