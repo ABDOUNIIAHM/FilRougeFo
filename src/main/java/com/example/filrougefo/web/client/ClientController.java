b9206378e07c6c6dad7df5d80300f18215534846
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-18 18:25:08 +0200

    added get post method to client controller

--- src/main/java/com/example/filrougefo/web/client/ClientController.java
+++ src/main/java/com/example/filrougefo/web/client/ClientController.java
@@ -1,21 +1,53 @@
 package com.example.filrougefo.web.client;
 
+import com.example.filrougefo.entity.Address;
+import com.example.filrougefo.entity.Client;
+import com.example.filrougefo.entity.PhoneNumber;
 import com.example.filrougefo.service.client.IntClientService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 @AllArgsConstructor
 public class ClientController {
     private IntClientService clientService;
+    private ClientMapper clientMapper;
 
     @GetMapping("client/register")
-    public String getRegisterForm(){
+    public ModelAndView getRegisterForm(){
 
-        return "registration-form";
+        ClientDto client = new ClientDto();
+        ModelAndView mav = new ModelAndView();
+
+        mav.addObject("client",client);
+        mav.addObject("address",new Address());
+        mav.addObject("phone",new PhoneNumber());
+        mav.setViewName("signup-form");
+
+        return mav;
     }
+    @PostMapping("client/register")
+    public String addNewClient(@ModelAttribute("client") ClientDto clientDto,
+                                     @ModelAttribute("address") Address address
+                                    ,@ModelAttribute("phone") PhoneNumber phone){
+
+
+        //ModelAndView mav = new ModelAndView();
+        clientDto.getAddressList().add(address);
+        clientDto.getPhoneNumberList().add(phone);
+
+        Client client = clientMapper.fromDTO(clientDto);
+        clientService.createClient(client);
+
+        return "success-signup";
+    }
+
+
 
 }
