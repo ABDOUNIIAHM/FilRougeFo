b84a459c2a63f68613268f3c4566db4fd0488f41
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-21 15:41:01 +0200

    updating order and added method in productcontroller

--- src/main/java/com/example/filrougefo/service/order/OrderService.java
+++ src/main/java/com/example/filrougefo/service/order/OrderService.java
@@ -1,10 +1,15 @@
 package com.example.filrougefo.service.order;
 import com.example.filrougefo.entity.Order;
+import com.example.filrougefo.entity.OrderLine;
+import com.example.filrougefo.entity.OrderStatus;
+import com.example.filrougefo.entity.Product;
 import com.example.filrougefo.exception.OrderNotFoundException;
+import com.example.filrougefo.repository.OrderLineRepository;
 import com.example.filrougefo.repository.OrderRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -12,6 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 public class OrderService implements IntOrderService{
     private final OrderRepository orderRepository;
+    private final OrderLineRepository orderLineRepository;
 
     @Override
     public List<Order> findAllOrdersByClientId(long id) {
@@ -38,5 +44,32 @@ public class OrderService implements IntOrderService{
         return orderRepository
                 .findById(id)
                 .orElseThrow(()-> new OrderNotFoundException("No such Order found for id:"+id));
+    }
+
+    @Override
+    public OrderLine addProductToOrder(Product product, int quantity) {
+
+        Order pendingOrder = hasPendingOrder();
+
+        OrderLine orderLine = new OrderLine();
+        orderLine.setOrder(pendingOrder);
+        orderLine.setProduct(product);
+        orderLine.setQuantity(BigDecimal.valueOf(quantity));
+
+        OrderLine newOrderLine = orderLineRepository.save(orderLine);
+
+        pendingOrder.getOrderLines().add(orderLine);
+        orderRepository.save(pendingOrder);
+
+        return newOrderLine;
+    }
+
+    private Order hasPendingOrder() {
+        List<Order> orders = orderRepository.findAllByStatus_Name("PENDING");
+        if(orders.size()==1){
+            return orders.get(0);
+        }
+        Order order = new Order();
+        return orderRepository.save(order);
     }
 }
--- src/main/java/com/example/filrougefo/web/Product/ProductController.java
+++ src/main/java/com/example/filrougefo/web/Product/ProductController.java
@@ -1,19 +1,25 @@
 package com.example.filrougefo.web.Product;
 
 import com.example.filrougefo.entity.Product;
+import com.example.filrougefo.exception.CategoryNotFoundException;
+import com.example.filrougefo.exception.ProductNotFoundException;
 import com.example.filrougefo.service.product.IntProductService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("/products/")
+@RequestMapping("/products")
 @AllArgsConstructor
 public class ProductController {
 
@@ -32,6 +38,31 @@ public class ProductController {
         model.addAttribute("productList", productDTOsList);
 
         return "product-list";
+    }
+
+    //pour tester la partie commande !
+    @GetMapping("/{id}")
+    public String detailProduct(Model model, @PathVariable int id){
+
+        ProductDTO productDto =productMapper.toDTO(productService.findById(id));
+        model.addAttribute("productDto",productDto);
+
+        return "detail-product";
+
+    }
+    //pour tester la partie commande !
+
+
+
+    @ExceptionHandler(value = {ProductNotFoundException.class})
+    public ModelAndView handleError(HttpServletRequest req, ProductNotFoundException ex) {
+
+        //logger.error("Request: " + req.getRequestURL() + " raised " + ex);
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("error");
+        mav.addObject("exception", ex);
+        mav.addObject("url", req.getRequestURL());
+        return mav;
     }
 
 }
