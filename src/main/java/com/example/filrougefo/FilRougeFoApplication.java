a4df1d86909d897fdf3d81d50c9ae85ef7c05599
ABDOUNIIAHM <abdellah.abdouni@gmail.com>
2023-05-29 15:59:42 +0200

    removed command line runner

--- src/main/java/com/example/filrougefo/FilRougeFoApplication.java
+++ src/main/java/com/example/filrougefo/FilRougeFoApplication.java
@@ -3,8 +3,9 @@ package com.example.filrougefo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class FilRougeFoApplication {
+public class FilRougeFoApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(FilRougeFoApplication.class, args);
