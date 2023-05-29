5c71c22824b6b399c9e6acf721f29701db31c4b5
mhewb <mhe.bousquet@gmail.com>
2023-05-29 13:47:05 +0200

    feat: added front and link to update password

--- src/main/java/com/example/filrougefo/FilRougeFoApplication.java
+++ src/main/java/com/example/filrougefo/FilRougeFoApplication.java
@@ -1,21 +1,7 @@
 package com.example.filrougefo;
 
-import com.example.filrougefo.entity.Client;
-import com.example.filrougefo.entity.Order;
-import com.example.filrougefo.entity.OrderLine;
-import com.example.filrougefo.repository.ClientRepository;
-import com.example.filrougefo.repository.OrderLineRepository;
-import com.example.filrougefo.repository.OrderRepository;
-import com.example.filrougefo.service.orderline.OrderLineService;
-import com.example.filrougefo.web.order.OrderLineMapper;
-import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
 public class FilRougeFoApplication {
